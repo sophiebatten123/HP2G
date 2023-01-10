ced20efa657ff181ebdf0b8e550898487d798181
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2023-01-10 15:45:49 +0000

    Adding service hover functionality to the site

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+document.getElementById('service-one').onmouseover = function() {
+    document.getElementById('service-one-text').style.display = "block";
+}
+
+document.getElementById('service-one').onmouseleave = function() {
+    document.getElementById('service-one-text').style.display = "none";
+}
+
+document.getElementById('service-two').onmouseover = function() {
+    document.getElementById('service-two-text').style.display = "block";
+}
+
+document.getElementById('service-two').onmouseleave = function() {
+    document.getElementById('service-two-text').style.display = "none";
+}
+
+document.getElementById('service-three').onmouseover = function() {
+    document.getElementById('service-three-text').style.display = "block";
+}
+
+document.getElementById('service-three').onmouseleave = function() {
+    document.getElementById('service-three-text').style.display = "none";
+}
