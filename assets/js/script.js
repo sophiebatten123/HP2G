dc1d002e8e71c653a545af2157d880ce21184b06
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2023-01-12 15:39:03 +0000

    Add main changes to the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,27 @@ document.getElementById('service-three').onmouseover = function() {
 document.getElementById('service-three').onmouseleave = function() {
     document.getElementById('service-three-text').style.display = "none";
 }
+
+document.getElementById('service-four').onmouseover = function() {
+    document.getElementById('service-four-text').style.display = "block";
+}
+
+document.getElementById('service-four').onmouseleave = function() {
+    document.getElementById('service-four-text').style.display = "none";
+}
+
+document.getElementById('service-five').onmouseover = function() {
+    document.getElementById('service-five-text').style.display = "block";
+}
+
+document.getElementById('service-five').onmouseleave = function() {
+    document.getElementById('service-five-text').style.display = "none";
+}
+
+document.getElementById('service-six').onmouseover = function() {
+    document.getElementById('service-six-text').style.display = "block";
+}
+
+document.getElementById('service-six').onmouseleave = function() {
+    document.getElementById('service-six-text').style.display = "none";
+}
