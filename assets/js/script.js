7380a8171de64875e4714c9e696b4c8431a88ee7
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2023-01-12 20:48:48 +0000

    Added about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,30 @@ document.getElementById('service-six').onmouseover = function() {
 document.getElementById('service-six').onmouseleave = function() {
     document.getElementById('service-six-text').style.display = "none";
 }
+
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHTML = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel__nav">
+            ${ buttonsHTML.join("") }
+        </div>
+    `);
+    
+    const buttons = carousel.querySelectorAll(".carousel__button");
+    
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            //unselect items
+            items.forEach(item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+
+            items[i].classList.add("carousel__item--selected");
+            button.classList.add("carousel__button--selected");
+        });
+    });
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
+});
