11b9a4e8b28240a2acc4f03a4798ac688336c19d
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2023-01-13 12:13:12 +0000

    Media queries for smaller screens has been set

--- assets/js/about.js
+++ assets/js/about.js
@@ -37,3 +37,51 @@ document.querySelectorAll(".carousel").forEach(carousel => {
     items[0].classList.add("carousel__item--selected");
     buttons[0].classList.add("carousel__button--selected");
 });
+
+document.getElementById('service-one').onmouseover = function() {
+    document.getElementById('service-one-text').style.display = "block";
+}
+
+document.getElementById('service-one').onmouseleave = function() {
+    document.getElementById('service-one-text').style.display = "none";
+}
+
+document.getElementById('service-two').onmouseover = function() {
+    document.getElementById('service-two-text').style.display = "block";
+}
+
+document.getElementById('service-two').onmouseleave = function() {
+    document.getElementById('service-two-text').style.display = "none";
+}
+
+document.getElementById('service-three').onmouseover = function() {
+    document.getElementById('service-three-text').style.display = "block";
+}
+
+document.getElementById('service-three').onmouseleave = function() {
+    document.getElementById('service-three-text').style.display = "none";
+}
+
+document.getElementById('service-four').onmouseover = function() {
+    document.getElementById('service-four-text').style.display = "block";
+}
+
+document.getElementById('service-four').onmouseleave = function() {
+    document.getElementById('service-four-text').style.display = "none";
+}
+
+document.getElementById('service-five').onmouseover = function() {
+    document.getElementById('service-five-text').style.display = "block";
+}
+
+document.getElementById('service-five').onmouseleave = function() {
+    document.getElementById('service-five-text').style.display = "none";
+}
+
+document.getElementById('service-six').onmouseover = function() {
+    document.getElementById('service-six-text').style.display = "block";
+}
+
+document.getElementById('service-six').onmouseleave = function() {
+    document.getElementById('service-six-text').style.display = "none";
+}
